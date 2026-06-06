830dca53844f16c4f11d48f99481be9859fe23e3
amanquij31-debug <amanquij31@gmail.com>
2026-06-06 16:36:30 -0500

    mejoras

--- js/script.js
+++ js/script.js
@@ -32,6 +32,28 @@ function sendMessage() {
         window.open("https://www.google.com","_blank");
     }
 
+    else if(message.toLowerCase() === "quien eres"){
+    response = "Soy JARVIS, tu asistente virtual.";
+    }
+
+    else if(message.toLowerCase() === "abre github"){
+    response = "Abriendo GitHub...";
+    window.open("https://github.com","_blank");
+    }
+
+    else if(message.toLowerCase() === "abre youtube"){
+    response = "Abriendo YouTube...";
+    window.open("https://youtube.com","_blank");
+    }
+
+    else if(message.toLowerCase() === "adios"){
+    response = "Hasta luego Junior.";
+    }
+
+    else if(message.toLowerCase() === "como estas"){
+    response = "Muy bien Junior.";
+    }
+
     chat.innerHTML += `
         <div class="jarvis">
             ${response}
@@ -42,3 +64,8 @@ function sendMessage() {
 
     chat.scrollTop = chat.scrollHeight;
 }
+document.getElementById("userInput").addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        sendMessage();
+    }
+});
